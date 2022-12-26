30c27e7c0148a0abc4e2e2bc8786abb150bad958
ThanhSonnTL <thanhson180301@gmail.com>
2022-12-26 15:52:15 +0700

    drawable

---
+++ app/src/main/java/com/sun/android/Drawables_style/ScorekeeperActivity.kt
@@ -0,0 +1,12 @@
+package com.sun.android.Drawables_style
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sun.android.R
+
+class ScorekeeperActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scorekeeper)
+    }
+}
